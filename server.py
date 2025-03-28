a6647adcfc58cab320acc6422d9d62fd237c23dd
Keerthan Shenoy <shenoyk2003@gmail.com>
2025-03-28 04:32:32 +0000

    Using Redis instead of dictionary

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, redirect, jsonify
 import string
 import random
+import redis
+import os
 
 app = Flask(__name__)
 
 # In-memory dictionary to store URL mappings
-url_mapping = {}
+redis_host = os.getenv("REDIS_HOST", "localhost")
+redis_port = int(os.getenv("REDIS_PORT", 6379))
+redis_client = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
 
 def generate_short_id(num_chars=6):
     """Generate a random string of fixed length."""
@@ -18,13 +22,13 @@ def shorten_url():
     if not long_url:
         return jsonify({'error': 'URL is required'}), 400
     short_id = generate_short_id()
-    url_mapping[short_id] = long_url
+    redis_client.set(short_id, long_url)
     short_url = request.host_url + short_id
     return jsonify({'short_url': short_url}), 201
 
 @app.route('/<short_id>')
 def redirect_url(short_id):
-    long_url = url_mapping.get(short_id)
+    long_url = redis_client.get(short_id)
     if long_url:
         return redirect(long_url)
     return jsonify({'error': 'Invalid URL'}), 404
